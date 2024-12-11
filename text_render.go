e7218432f1b915bfcdf42d0baf861627966b9c65
rom-vtn <romahvito@gmail.com>
2024-12-12 00:19:45 +0100

    extend renderer to allow runes >256

--- text_render.go
+++ text_render.go
@@ -1,7 +1,17 @@
 package gomax7219
 
-// NewBytesTextRender renders the given code points (byte per byte) using the given code page
+// NewBytesTextRender renders the given code points (byte per byte) using the given code page.
+// This requires all codepoints to fit in a signle byte (not compatible with >255 codepoints)
 func NewBytesTextRender(codePage [][]byte, codePoints []byte) StaticGrid {
+	var render StaticGrid
+	for _, codePoint := range codePoints {
+		render = append(render, codePage[codePoint]...)
+	}
+	return render
+}
+
+// NewRunesTextRender renders text as a series of runes, but allows runes to be >=256
+func NewRunesTextRender(codePage [][]byte, codePoints []rune) StaticGrid {
 	var render StaticGrid
 	for _, codePoint := range codePoints {
 		render = append(render, codePage[codePoint]...)
@@ -12,10 +22,17 @@ func NewBytesTextRender(codePage [][]byte, codePoints []byte) StaticGrid {
 // NewStringTextRender renders the given string (converting it to runes, then to bytes) using the given code page
 func NewStringTextRender(codePage [][]byte, content string) StaticGrid {
 	//convert runes to bytes one by one
+	needsTruncating := false
 	var asBytes []byte
 	for _, r := range content {
+		if len(codePage) <= int(r) {
+			needsTruncating = true
+		}
 		asBytes = append(asBytes, byte(r))
 	}
 
-	return NewBytesTextRender(codePage, asBytes)
+	if needsTruncating {
+		return NewBytesTextRender(codePage, asBytes)
+	}
+	return NewRunesTextRender(codePage, []rune(content))
 }
